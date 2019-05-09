b60219611c4d70f81590e19a474960deea44157d
Kevin Wong <kewong@transperfect.com>
2019-05-09 01:45:04 -0400

    style fixes

--- chatbot-frontend/src/components/Message.js
+++ chatbot-frontend/src/components/Message.js
@@ -12,25 +12,27 @@ class Message extends React.Component {
 
 	render(){
 		return (
-			<div>
-			<ul className="chats">
-			{this.props.messages.map((messages, idx) => {
-				return (	
-				<li className={`chat ${botName === messages.username ? "left" : "right"}`} key={idx}>
-					<div>
-						{messages.username}
-					</div>
-					<div>
-						{messages.content}
-					</div>
-				</li>
-				)
-			})}
-			</ul>
-			<form className="input" onSubmit={(e)=>{e.preventDefault();this.props.onSubmit()}}>
-				<input type="text" ref={this.msg}/>
-				<input type="submit" value="Submit"/>
-			</form>
+			<div class="message-area">
+				<div class="chats-container">
+					<ul className="chats">
+					{this.props.messages.map((messages, idx) => {
+						return (	
+						<li className={`chat ${botName === messages.username ? "left" : "right"}`} key={idx}>
+							<div>
+								{messages.username}
+							</div>
+							<div>
+								{messages.content}
+							</div>
+						</li>
+						)
+					})}
+					</ul>
+				</div>
+				<form className="input" onSubmit={(e)=>{e.preventDefault();this.props.onSubmit()}}>
+					<input type="text" ref={this.msg}/>
+					<input type="submit" value="Submit"/>
+				</form>
 			</div>
 		);
 	}
